ee806223616e20cf47096b2b2b4299ffb76b8bfa
PunpunOnodera <89903236+PunpunOnodera@users.noreply.github.com>
2022-05-30 20:34:59 +0700

    Create RandomizingMethod.java

---
+++ RandomizingMethod.java
@@ -0,0 +1,16 @@
+//masukkan ke class yang ada methodnya, atau di public void main
+//pakai import java.util.*;
+public double random(){
+  Random rand = new Random();
+  double a = rand.nextDouble(1);
+  if(a){
+    double b = rand.nextDouble(10) + 1;
+    return b;
+  }else{
+    return a;
+  }
+  
+  //metode call ada di bawah
+  //coba ganti elemen di array dg call
+  
+  
